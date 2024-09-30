2291bed87270bde830405d40003c79aef62f3bd2
Nikita Pavlov <pavlovnp@outlook.com>
2024-09-30 16:16:47 +0200

    init

---
+++ cmd/storage/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/npavlov/storage-go/internal/storage"
+)
+
+func main() {
+	fmt.Println("Hello World")
+
+	st := storage.NewStorage()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/npavlov/storage-go
+
+go 1.23.1
